a9b83cc717090adc94bcdf22304a3de3bf1f14a0
360moscowphotos <360moscowphotos@gmail.com>
2025-09-01 15:17:46 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,169 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "Ванная",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.5117097203512309,
+        "pitch": 0.12003533838751679,
+        "fov": 1.056731189113842
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.584666048295311,
+          "pitch": 0.9315538116124742,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-",
+      "name": "Комната",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.8142908947183383,
+        "pitch": 0.4027981466741348,
+        "fov": 1.056731189113842
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.6377270284731633,
+          "pitch": 0.5088436142446664,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "Прихожая",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.876786121835405,
+        "pitch": 0.3900421694718794,
+        "fov": 1.056731189113842
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.395902363082933,
+          "pitch": 0.6743106348069698,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": -2.3890994891245363,
+          "pitch": 0.7469221497255418,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-",
+      "name": "Кухня",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.41422335309365,
+        "pitch": 0.3002348760389264,
+        "fov": 1.056731189113842
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2491626480181584,
+          "pitch": 0.6106268959702668,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Квартира",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
